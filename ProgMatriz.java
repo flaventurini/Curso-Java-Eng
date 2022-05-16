96159d74f59db97e0a1c6964e7568e06d24a664c
flaventurini <100423453+flaventurini@users.noreply.github.com>
2022-05-16 15:41:36 -0300

    Updated

--- ProgMatriz.java
+++ ProgMatriz.java
@@ -7,13 +7,13 @@ class ProgMatriz
   {
     //declaração de variáveis
     int vetor[] = {2, 4, 6, 8, 10};
-    int matriz[][] = new int [2][3];
+    int matriz[][] = new int [3][4];
     String msg="vetor = ";
 
     //vetor = :
     for (int i=0; i<vetor.length; i++) 
     {
-      msg = msg + vetor[i] + " ";
+      msg = msg + vetor[i] + "  ";
     }
     JOptionPane.showMessageDialog(null, msg);
 
@@ -23,7 +23,7 @@ class ProgMatriz
       for (int j=0; j<matriz.length; j++)
       {
         matriz[i][j] = Integer.parseInt(JOptionPane.showInputDialog("Digite um número inteiro para posição " + i + " e " + j));
-        msg = msg + matriz[i][j];
+        msg = msg + matriz[i][j] + "  ";
       }
       msg = msg + "\n";
     }
